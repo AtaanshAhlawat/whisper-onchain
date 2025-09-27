dea241258a4b988517ec543db78d685ca8b851f8
Atanshu Ahlawat <atanshu.ahlawat.07@gmail.com>
2025-09-28 00:16:37 +0530

    Changes in frontend.

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
+  title: "Whisper Onchain",
   description: "Built with 🏗 Scaffold-ETH 2",
 });
 
